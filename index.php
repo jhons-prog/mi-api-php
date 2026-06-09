c3614ea73f586776e93911ea388a645ccf4de05d
jhons-prog <jhonnoroelmamanicruz@gmail.com>
2026-06-08 20:17:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
-echo json_encode([
-    "mysqli" => extension_loaded('mysqli'),
-    "pdo_mysql" => extension_loaded('pdo_mysql'),
-    "host" => getenv('mysql.railway.internal'),
-    "port" => getenv('3306'),
-    "db" => getenv('railway'),
-    "user" => getenv('root')
-]);
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+
+$host   = "mysql.railway.internal";
+$dbname = "railway";
+$user   = "root";
+$pass   = "JgTKMgtAGsGaNDaLJpIeAPIqMJtnmJpI";
+$port   = 3306;
+
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+if (!$conn) {
+    echo json_encode(["error" => mysqli_connect_error()]);
+    exit;
+}
+
+$result = mysqli_query($conn, "SELECT * FROM productos");
+$data = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE);
+mysqli_close($conn);
 ?>
