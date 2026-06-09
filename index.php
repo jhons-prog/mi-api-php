47d278e5cc9ffb031e741231d5bb8d30e2dafd2c
jhons-prog <jhonnoroelmamanicruz@gmail.com>
2026-06-08 19:52:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
-header("Content-Type: application/json; charset=utf-8");
-header("Access-Control-Allow-Origin: *");
-
-$host     = getenv('mysql.railway.internal');
-$dbname   = getenv('railway');
-$user     = getenv('root');
-$pass     = getenv('JgTKMgtAGsGaNDaLJpIeAPIqMJtnmJpI');
-$port     = getenv('3306');
-
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $pdo->query("SELECT * FROM productos");
-    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($data, JSON_UNESCAPED_UNICODE);
-} catch (Exception $e) {
-    echo json_encode(["error" => $e->getMessage()]);
-}
+echo json_encode([
+    "extensions" => get_loaded_extensions(),
+    "pdo_drivers" => PDO::getAvailableDrivers()
+]);
 ?>
