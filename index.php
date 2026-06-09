c84a4db6b986dd52e5a7d74f96c960a66ed23aed
jhons-prog <jhonnoroelmamanicruz@gmail.com>
2026-06-08 20:11:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 echo json_encode([
     "mysqli" => extension_loaded('mysqli'),
     "pdo_mysql" => extension_loaded('pdo_mysql'),
-    "host" => getenv('MYSQLHOST'),
-    "port" => getenv('MYSQLPORT'),
-    "db" => getenv('MYSQLDATABASE'),
-    "user" => getenv('MYSQLUSER')
+    "host" => getenv('mysql.railway.internal'),
+    "port" => getenv('3306'),
+    "db" => getenv('railway'),
+    "user" => getenv('root')
 ]);
 ?>
