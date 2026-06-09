c9551a220c0e9261b197924a6cf908615ec1de06
jhons-prog <jhonnoroelmamanicruz@gmail.com>
2026-06-08 19:48:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 header("Content-Type: application/json; charset=utf-8");
 header("Access-Control-Allow-Origin: *");
 
-$host     = getenv('MYSQL_HOST');
-$dbname   = getenv('MYSQL_DATABASE');
-$user     = getenv('MYSQL_USER');
-$pass     = getenv('MYSQL_PASSWORD');
-$port     = getenv('MYSQL_PORT') ?: 3306;
+$host     = getenv('mysql.railway.internal');
+$dbname   = getenv('railway');
+$user     = getenv('root');
+$pass     = getenv('JgTKMgtAGsGaNDaLJpIeAPIqMJtnmJpI');
+$port     = getenv('3306');
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
